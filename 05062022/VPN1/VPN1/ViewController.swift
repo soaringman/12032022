379c31c48b7ab63d41740c855f5bd069f077aa87
Alexey Gulyaev <ghost-shadows@yandex.ru>
2022-06-28 20:46:07 +0300

    Не смог разместить Свитч на оси х по центру

--- 05062022/VPN1/VPN1/ViewController.swift
+++ 05062022/VPN1/VPN1/ViewController.swift
@@ -19,42 +19,42 @@ class ViewController: UIViewController {
     }()
 
     private lazy var textLabel: UIView = {
-        let view = UILabel()
-        view.textColor = .black
-        view.font = .systemFont(ofSize: 12)
-        view.textAlignment = .center
-        view.text = "Отключено"
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+        let textLabel = UILabel()
+        textLabel.textColor = .black
+        textLabel.font = .systemFont(ofSize: 15)
+        textLabel.textAlignment = .center
+        textLabel.text = "Отключено"
+        textLabel.translatesAutoresizingMaskIntoConstraints = false
+        return textLabel
     }()
     
     private lazy var countDownLabel: UILabel = {
-        let view = UILabel()
-        view.textColor = .black.withAlphaComponent(0.1)
-        view.font = .systemFont(ofSize: 20)
-        view.textAlignment = .center
-        view.text = "00:00:00"
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+        let countDownLabel = UILabel()
+        countDownLabel.textColor = .black.withAlphaComponent(0.1)
+        countDownLabel.font = .systemFont(ofSize: 34)
+        countDownLabel.textAlignment = .center
+        countDownLabel.text = "00:00:00"
+        countDownLabel.translatesAutoresizingMaskIntoConstraints = false
+        return countDownLabel
     }()
     
     private lazy var switcher: UISwitch = {
-        let view = UISwitch()
-        view.backgroundColor = .white
-        view.tintColor = .link
-        view.translatesAutoresizingMaskIntoConstraints = false
-        return view
+        let switcher = UISwitch()
+        switcher.thumbTintColor = .link
+        switcher.onTintColor = .systemGray2
+        switcher.transform = CGAffineTransform(scaleX: 3.5, y: 3.5)
+        switcher.translatesAutoresizingMaskIntoConstraints = false
+        return switcher
     }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setUpUI()
-        // Do any additional setup after loading the view.
     }
 
     private func setUpUI() {
-        view.backgroundColor = .black
+        view.backgroundColor = .white
         stack.addArrangedSubview(textLabel)
         stack.addArrangedSubview(countDownLabel)
         
@@ -62,49 +62,15 @@ class ViewController: UIViewController {
         view.addSubview(switcher)
 
         
-        stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 110.5).isActive = true
-        stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 109.5).isActive = true
-        stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -20).isActive = true
-        
-        switcher.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 110.5).isActive = true
-        switcher.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 109.5).isActive = true
-        switcher.topAnchor.constraint(equalTo: stack.bottomAnchor, constant: -40).isActive = true
-        
-        
-        
-        
-        
-        
-        
-        
-        
-//        textLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        textLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-        
-        //не уверен что нужно указывать принудительно ширину и высоту поскольку
-        //если текст окажется чуть больше он просто не отобразиться или будет виден только кусок текста
-//        textLabel.heightAnchor.constraint(equalToConstant: 22)
-//        textLabel.widthAnchor.constraint(equalToConstant: 82)
-//        textLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: -252).isActive = true
-//        textLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: -1780).isActive = true
-        
-        
-        
-//        myView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-//        myView.centerYAnchor.constraint(equalTo: view.centerYAnchor).isActive = true
-//        myView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-//        myView.heightAnchor.constraint(equalToConstant: 200).isActive = true
-//
-//        bottomView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16).isActive = true
-//        bottomView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16).isActive = true
-//
-//        bottomView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
-//
-//        bottomView.topAnchor.constraint(equalTo: myView.bottomAnchor, constant: 10).isActive = true
-//
+        stack.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 110).isActive = true
+        stack.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -110).isActive = true
+        stack.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 287).isActive = true
+  
+        switcher.topAnchor.constraint(equalTo: stack.bottomAnchor, constant: 80).isActive = true
+        switcher.trailingAnchor.constraint(equalTo: stack.trailingAnchor, constant: -110).isActive = true
+        switcher.leadingAnchor.constraint(equalTo: stack.leadingAnchor, constant: 110).isActive = true
+
     }
-
-
 }
 
 
